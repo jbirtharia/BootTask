e27b761c1622c2e5178205ac24332e7f1683b48d
nitinParkway <47175248+nitinParkway@users.noreply.github.com>
2020-04-26 12:29:38 +0530

    Deleting unused dependencies

--- src/test/java/com/phoenix/boottask/BootTaskApplicationTests.java
+++ src/test/java/com/phoenix/boottask/BootTaskApplicationTests.java
@@ -1,13 +1,24 @@
 package com.phoenix.boottask;
 
-import org.junit.jupiter.api.Test;
+import com.phoenix.boottask.controllers.TrackerController;
+import com.phoenix.boottask.service.ApplicationService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
+@WebMvcTest(TrackerController.class)
 class BootTaskApplicationTests {
+
+	@Autowired
+	MockMvc mvc;
+
+	@MockBean
+	ApplicationService applicationService;
 
 	@Test
 	void contextLoads() {
